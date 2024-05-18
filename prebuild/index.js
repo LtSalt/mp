d912349e09cea584a259a1122ab43cdb3974a281
David Will <64697174+LtSalt@users.noreply.github.com>
2024-05-18 13:33:13 +0200

    writing & reading data to/from file system

--- prebuild/index.js
+++ prebuild/index.js
@@ -1,14 +1,30 @@
+import * as fsPromises from "fs/promises";
+import * as path from "path";
 import decrypt from "./decrypt.js";
 import createClients from "./createClients.js"
 import getDB  from './getDB.js';
 import getContent from "./getContent.js";
 
 export const onPreBuild = async() => {
-    const credentials = decrypt(process.env)
-    const clients = await createClients(credentials)
+    try {
+        const credentials = decrypt(process.env)
+        const clients = await createClients(credentials)
+        
+        const [db, content] = await Promise.all([
+            getDB(clients, process.env),
+            getContent(clients, process.env)
+        ])
+        
+        const pathDir = path.join(process.cwd(), 'static', 'data')
+        const pathDB = path.join(process.cwd(), 'static', "data", "db.json") 
+        const pathContent = path.join(process.cwd(), 'static', "data", "content.json") 
 
-    const [db, content] = await Promise.all([
-        getDB(clients, process.env),
-        getContent(clients, process.env)
-    ])
+        await fsPromises.mkdir(pathDir)
+        await Promise.all([
+            fsPromises.writeFile(pathDB, JSON.stringify(db)),
+            fsPromises.writeFile(pathContent, JSON.stringify(content))
+        ])
+    } catch(err) {
+        console.error(err)
+    }
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
+<script>
+	import db from '/static/data/db.json';
+	import content from '/static/data/content.json';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button on:click={() => console.log(db)}>Show DB</button>
+<button on:click={() => console.log(content)}>Show Content</button>
